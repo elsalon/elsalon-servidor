9e34d0d6ac426818b3ad7d1eb86fa7db8a40e165
gonza moiguer <gonzamoiguer@gmail.com>
2024-10-17 18:46:01 -0300

    Agregado APIs para entrar y salir de comisiones individual + grupalmente

--- src/collections/Comisiones.ts
+++ src/collections/Comisiones.ts
@@ -30,23 +30,132 @@ const Comisiones: CollectionConfig = {
             hasMany: true,
         },
         {
+            name: 'grupos',
+            type: 'relationship',
+            relationTo: 'grupos',
+            hasMany: true,
+        },
+        {
             name: 'contexto',
             type: 'relationship',
             relationTo: 'salones',
         },
         ...SlugField(
             {
-              name: 'slug',
-              admin: {
-                position: 'sidebar',
-              },
+                name: 'slug',
+                admin: {
+                    position: 'sidebar',
+                },
             },
             {
-                appendOnDuplication : true,
+                appendOnDuplication: true,
                 useFields: ['nombre'],
             },
         ),
     ],
+    endpoints: [
+        {
+            path: '/:comisionid/unirme',
+            method: 'patch',
+            handler: async (req, res, next) => {
+                try {
+
+                    if (!req.user) return res.status(401).json({ error: 'Unauthorized' });
+                    const { comisionid } = req.params;
+                    const { grupoId } = req.body;
+                    const userId = req.user?.id; // Obteniendo el ID del usuario actual
+                    const comision = await req.payload.findByID({
+                        collection: 'comisiones',
+                        id: comisionid,
+                        depth: 0,
+                    });
+
+                    console.log({
+                        collection: 'grupos',
+                        id: grupoId,
+                    })
+
+                    if (grupoId) {
+                        // Quiere unir a su grupo
+                        const grupo = await req.payload.findByID({
+                            collection: 'grupos',
+                            id: grupoId,
+                        });
+                        if (!grupo) return res.status(404).json({ error: 'Grupo no encontrado' });
+                        comision.grupos.push(grupo.id);
+                    } else {
+                        // Se quiere unir individualmente
+                        const esAlumno = req.user.rol === 'alumno';
+                        if (esAlumno) {
+                            // Si es alumno, se le agrega a los integrantes
+                            comision.integrantes.push(userId);
+                        } else {
+                            // Si es docente, se le agrega a los docentes
+                            comision.docentes.push(userId);
+                        }
+                    }
+
+                    const update = await req.payload.update({
+                        collection: 'comisiones',
+                        id: comisionid,
+                        data: comision,
+                    });
+                    return res.status(200).json(update);
+                } catch (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Error interno' });
+                }
+            }
+        },
+        {
+            path: '/:comisionid/abandonar',
+            method: 'patch',
+            handler: async (req, res, next) => {
+                try {
+                    if (!req.user) return res.status(401).json({ error: 'Unauthorized' });
+                    const { comisionid } = req.params;
+                    const { grupoId } = req.body;
+                    const userId = req.user?.id; // Obteniendo el ID del usuario actual
+                    const comision = await req.payload.findByID({
+                        collection: 'comisiones',
+                        id: comisionid,
+                        depth: 0,
+                    });
+
+                    console.log("GRUPO", grupoId, req.body)
+                    if (grupoId) {
+                        // Quiere abandonar un grupo
+                        const grupo = await req.payload.findByID({
+                            collection: 'grupos',
+                            id: grupoId,
+                        });
+                        if (!grupo) return res.status(404).json({ error: 'Grupo no encontrado' });
+                        comision.grupos = comision.grupos.filter(g => g !== grupo.id);
+                    } else {
+                        // Se quiere ir individualmente
+                        const esAlumno = req.user.rol === 'alumno';
+                        if (esAlumno) {
+                            // Si es alumno, se lo saca de integrantes
+                            comision.integrantes = comision.integrantes.filter(i => i !== userId);
+                        } else {
+                            // Si es docente, se lo saca de docentes
+                            comision.docentes = comision.docentes.filter(d => d !== userId);
+                        }
+                    }
+
+                    const update = await req.payload.update({
+                        collection: 'comisiones',
+                        id: comisionid,
+                        data: comision,
+                    });
+                    return res.status(200).json(update);
+                } catch (err) {
+                    console.error(err);
+                    return res.status(500).json({ error: 'Error interno' });
+                }
+            }
+        }
+    ]
 }
 
 export default Comisiones;
