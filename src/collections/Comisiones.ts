f548f9c67562816904f37bff3f0528cf7ee5969d
gonza moiguer <gonzamoiguer@gmail.com>
2024-10-17 19:55:13 -0300

    Agregue endpoint feed para comisiones, porque tiene un query complejo de hacer desde front

--- src/collections/Comisiones.ts
+++ src/collections/Comisiones.ts
@@ -1,6 +1,7 @@
 import { SlugField } from '@nouance/payload-better-fields-plugin'
 import { CollectionConfig } from 'payload/types'
 import { isAdminOrDocente } from '../helper'
+import { unirme, abandonar, feed } from './ComisionesEndpoints'
 
 const Comisiones: CollectionConfig = {
     slug: 'comisiones',
@@ -55,105 +56,19 @@ const Comisiones: CollectionConfig = {
     ],
     endpoints: [
         {
+            path: '/:comisionid/feed',
+            method: 'get',
+            handler: feed,
+        },
+        {
             path: '/:comisionid/unirme',
             method: 'patch',
-            handler: async (req, res, next) => {
-                try {
-
-                    if (!req.user) return res.status(401).json({ error: 'Unauthorized' });
-                    const { comisionid } = req.params;
-                    const { grupoId } = req.body;
-                    const userId = req.user?.id; // Obteniendo el ID del usuario actual
-                    const comision = await req.payload.findByID({
-                        collection: 'comisiones',
-                        id: comisionid,
-                        depth: 0,
-                    });
-
-                    console.log({
-                        collection: 'grupos',
-                        id: grupoId,
-                    })
-
-                    if (grupoId) {
-                        // Quiere unir a su grupo
-                        const grupo = await req.payload.findByID({
-                            collection: 'grupos',
-                            id: grupoId,
-                        });
-                        if (!grupo) return res.status(404).json({ error: 'Grupo no encontrado' });
-                        comision.grupos.push(grupo.id);
-                    } else {
-                        // Se quiere unir individualmente
-                        const esAlumno = req.user.rol === 'alumno';
-                        if (esAlumno) {
-                            // Si es alumno, se le agrega a los integrantes
-                            comision.integrantes.push(userId);
-                        } else {
-                            // Si es docente, se le agrega a los docentes
-                            comision.docentes.push(userId);
-                        }
-                    }
-
-                    const update = await req.payload.update({
-                        collection: 'comisiones',
-                        id: comisionid,
-                        data: comision,
-                    });
-                    return res.status(200).json(update);
-                } catch (err) {
-                    console.error(err);
-                    return res.status(500).json({ error: 'Error interno' });
-                }
-            }
+            handler: unirme
         },
         {
             path: '/:comisionid/abandonar',
             method: 'patch',
-            handler: async (req, res, next) => {
-                try {
-                    if (!req.user) return res.status(401).json({ error: 'Unauthorized' });
-                    const { comisionid } = req.params;
-                    const { grupoId } = req.body;
-                    const userId = req.user?.id; // Obteniendo el ID del usuario actual
-                    const comision = await req.payload.findByID({
-                        collection: 'comisiones',
-                        id: comisionid,
-                        depth: 0,
-                    });
-
-                    console.log("GRUPO", grupoId, req.body)
-                    if (grupoId) {
-                        // Quiere abandonar un grupo
-                        const grupo = await req.payload.findByID({
-                            collection: 'grupos',
-                            id: grupoId,
-                        });
-                        if (!grupo) return res.status(404).json({ error: 'Grupo no encontrado' });
-                        comision.grupos = comision.grupos.filter(g => g !== grupo.id);
-                    } else {
-                        // Se quiere ir individualmente
-                        const esAlumno = req.user.rol === 'alumno';
-                        if (esAlumno) {
-                            // Si es alumno, se lo saca de integrantes
-                            comision.integrantes = comision.integrantes.filter(i => i !== userId);
-                        } else {
-                            // Si es docente, se lo saca de docentes
-                            comision.docentes = comision.docentes.filter(d => d !== userId);
-                        }
-                    }
-
-                    const update = await req.payload.update({
-                        collection: 'comisiones',
-                        id: comisionid,
-                        data: comision,
-                    });
-                    return res.status(200).json(update);
-                } catch (err) {
-                    console.error(err);
-                    return res.status(500).json({ error: 'Error interno' });
-                }
-            }
+            handler: abandonar,
         }
     ]
 }
---
+++ src/collections/ComisionesEndpoints.js
@@ -0,0 +1,166 @@
+import create from "payload/dist/collections/operations/create";
+
+export const unirme = async (req, res, next) => {
+    try {
+
+        if (!req.user) return res.status(401).json({ error: 'Unauthorized' });
+        const { comisionid } = req.params;
+        const { grupoId } = req.body;
+        const userId = req.user?.id; // Obteniendo el ID del usuario actual
+        const comision = await req.payload.findByID({
+            collection: 'comisiones',
+            id: comisionid,
+            depth: 0,
+        });
+
+        // TODO CHEQUEAR SI EL USUARIO YA ESTA EN OTRA COMISION DE ESTE CONTEXTO
+
+        if (grupoId) {
+            // Quiere unir a su grupo
+            const grupo = await req.payload.findByID({
+                collection: 'grupos',
+                id: grupoId,
+            });
+            if (!grupo) return res.status(404).json({ error: 'Grupo no encontrado' });
+            comision.grupos.push(grupo.id);
+        } else {
+            // Se quiere unir individualmente
+            const esAlumno = req.user.rol === 'alumno';
+            if (esAlumno) {
+                // Si es alumno, se le agrega a los integrantes
+                comision.integrantes.push(userId);
+            } else {
+                // Si es docente, se le agrega a los docentes
+                comision.docentes.push(userId);
+            }
+        }
+
+        const update = await req.payload.update({
+            collection: 'comisiones',
+            id: comisionid,
+            data: comision,
+        });
+        return res.status(200).json(update);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error interno' });
+    }
+};
+
+export const abandonar = async (req, res, next) => {
+    try {
+        if (!req.user) return res.status(401).json({ error: 'Unauthorized' });
+        const { comisionid } = req.params;
+        const { grupoId } = req.body;
+        const userId = req.user?.id; // Obteniendo el ID del usuario actual
+        const comision = await req.payload.findByID({
+            collection: 'comisiones',
+            id: comisionid,
+            depth: 0,
+        });
+
+        if (grupoId) {
+            // Quiere abandonar un grupo
+            const grupo = await req.payload.findByID({
+                collection: 'grupos',
+                id: grupoId,
+            });
+            if (!grupo) return res.status(404).json({ error: 'Grupo no encontrado' });
+            comision.grupos = comision.grupos.filter(g => g !== grupo.id);
+        } else {
+            // Se quiere ir individualmente
+            const esAlumno = req.user.rol === 'alumno';
+            if (esAlumno) {
+                // Si es alumno, se lo saca de integrantes
+                comision.integrantes = comision.integrantes.filter(i => i !== userId);
+            } else {
+                // Si es docente, se lo saca de docentes
+                comision.docentes = comision.docentes.filter(d => d !== userId);
+            }
+        }
+
+        const update = await req.payload.update({
+            collection: 'comisiones',
+            id: comisionid,
+            data: comision,
+        });
+        return res.status(200).json(update);
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error interno' });
+    }
+};
+
+export const feed = async (req, res, next) => {
+    try{
+        if (!req.user) return res.status(401).json({ error: 'Unauthorized' });
+
+        const {startDate, endDate, page, createdGreaterThan} = req.query;
+        
+        const comision = await req.payload.findByID({
+            collection: 'comisiones',
+            id: req.params.comisionid,
+            depth: 0,
+        });
+
+        if (!comision) return res.status(404).json({ error: 'Comisión no encontrada' });
+
+        /*
+        Cosas que vamos a buscar en este feed:
+        * Entradas al espacio donde está esta comision (comision.contexto)
+            * Que estén en el rango de fechas (startDate, endDate) que son del actual periodo (cuatri o anual)
+                * De usuarios que estén en la comisión (comision.integrantes & docentes)
+                ó
+                * De grupos cuyos integrantes estén en la comision 
+        */
+
+        let query = {
+            and:[
+                {
+                    createdAt: { greater_than_equal: startDate },
+                },
+                {
+                    createdAt: { less_than_equal: endDate },
+                },
+                {
+                    sala: { equals: comision.contexto }
+                },
+                {
+                    or: [
+                        {
+                            autor: { in: comision.integrantes }
+                        },
+                        {
+                            'grupo.integrantes': { in: comision.integrantes }
+                        },
+                    ]
+                }
+            ]
+        }
+        
+        if(createdGreaterThan){
+            // Agrego la fecha de creación como criterio de busqueda
+            query = {
+                and: [
+                    query,
+                    {
+                        createdAt: { greater_than: new Date(createdGreaterThan) }
+                    }
+                ]
+            }
+        }
+
+        const feed = await req.payload.find({
+            collection: 'entradas',
+            where: query,
+            sort: "-createdAt",  // Ordenar por fecha de creación, de más reciente a más antigua
+            limit: 5,
+            page: parseInt(page) || 1,
+        });
+
+        return res.status(200).json(feed);
+    }catch(err){
+        console.error(err);
+        return res.status(500).json({ error: 'Error interno' });
+    }
+}
