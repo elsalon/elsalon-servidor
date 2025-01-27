5db37ff0a149f7a1b77d1c3656e842f975ae383f
gonza moiguer <gonzamoiguer@gmail.com>
2025-01-27 20:32:36 -0300

    Agregado script de migracion que marca todas las entradas como publicadas

---
+++ src/migrations/20250127_204636_versions_plugin.ts
@@ -0,0 +1,30 @@
+import {
+  MigrateUpArgs,
+  MigrateDownArgs,
+} from "@payloadcms/db-mongodb";
+
+export async function up({ payload }: MigrateUpArgs): Promise<void> {
+  // Migration code
+  console.log('Updating all entradas to published');
+  const res = await payload.update({ 
+    collection: 'entradas',
+    where: {
+      _status: {
+        exists: {equals: false},
+      },
+    },
+    depth: 0,
+    data: {
+      _status: 'published', 
+    },
+    context: {
+      // set a flag to prevent extra
+      skipHooks: true,
+    },
+  });
+  console.log(res);
+};
+
+export async function down({ payload }: MigrateDownArgs): Promise<void> {
+  // Migration code
+};
